8e8ea22106b6c90d91748fcb5da4cffe94117449
Max Hanson <mthanson2@gmail.com>
2023-06-02 11:28:22 -0700

    Built navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Image, Text } from "@chakra-ui/react"
+
+const NavBar = () => {
+    return (
+        <HStack>
+            <Image></Image>
+            <Text>NavBar</Text>
+        </HStack>
+    )
+}
+
+export default NavBar
